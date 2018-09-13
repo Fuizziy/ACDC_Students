0da3c1a0ecbea7ff021477bc26caf202e15a2ac0
Antoine Allard <antoine.allard@epita.fr>
2018-09-13 21:31:17 +0200

    Ended students save, implementing penalties

--- ACDC_Students/src/fr/fuizziy/acdc/Main.java
+++ ACDC_Students/src/fr/fuizziy/acdc/Main.java
@@ -1,20 +1,24 @@
 package fr.fuizziy.acdc;
   
-import java.io.File;
-import java.io.FileReader;
+import java.io.File; 
 import java.io.IOException; 
 
 public class Main {
 	
+	public static File save;
+	
 	public static void main(String[] args) throws IOException { 
 		System.out.println("WELCOME. How will you annoy the sups today ?");  
-		File file = new File("scores.txt"); 
-		if (file.createNewFile() || file.length() == 0) {
+		save = new File("scores.txt"); 
+		if (save.createNewFile() || save.length() == 0) {
 			System.out.println("Generating new scores file...");
 			System.out.println("Please configure it and come back.");
 			return;
 		}
-		Student.load_students(file); 
+		Student.load_students(save); 
+		Student.save_students(save);
 	} 
+	
+	
 	 
 }
--- ACDC_Students/src/fr/fuizziy/acdc/Student.java
+++ ACDC_Students/src/fr/fuizziy/acdc/Student.java
@@ -1,16 +1,18 @@
 package fr.fuizziy.acdc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Student {
 
-	public static Set<Student> students_class = new HashSet<Student>();
-	
+	public static Set<Student> students_class = new HashSet<Student>(); 
 	public static void load_students(File file) throws IOException {
 		FileReader file_reader = new FileReader(file);
 		BufferedReader bufferedReader = new BufferedReader(file_reader); 
@@ -19,26 +21,59 @@ public class Student {
 			String[] student_values = line.split(" ");
 			Student student = new Student(student_values[0],
 					Short.parseShort(student_values[1]),
-					Short.parseShort(student_values[2]));
-			System.out.println(student.loadMessage());
+					Short.parseShort(student_values[2]),
+					student_values[3]);
+			System.out.println("loaded " + student.format()
+			+ ". Penalities: " + student.convert_penalties());
+			
 		} 
 		bufferedReader.close();
 		file_reader.close();
 	}
-	
-	String login;
-	short asked_times = 0;
-	short good_answers = 0;
-	
-	public Student(String login, short asked_times, short good_answers) {
-		this.login = login;
-		this.good_answers = good_answers;
-		this.asked_times = asked_times;
-		students_class.add(this);
+	public static void save_students(File file) throws IOException {
+		new PrintWriter(file).close();
+		FileWriter fw = new FileWriter(file.getName(), true); 
+	    BufferedWriter bw = new BufferedWriter(fw); 
+	    PrintWriter pw = new PrintWriter(bw);
+	    for (Student student : students_class)
+	    	student.save(pw);
+	    pw.close();
+	    bw.close();
+	    fw.close();
 	}
 	
-	public String loadMessage() {
-		return "Loaded " + this.login + " with " + good_answers + "/" +
+	private String login;
+	private short asked_times, good_answers;
+	public int[] penalties;
+	
+	public Student(String login, short asked_times, short good_answers,
+			String str_penalty) {
+		this.login = login;
+		this.good_answers = good_answers; 
+		this.asked_times = asked_times;
+		this.penalties = parse_penalties(str_penalty);
+		students_class.add(this);
+		this.answer(true);
+	}
+	
+	public int[] parse_penalties(String str) {
+		int[] result = new int[4]; 
+		int index = 0;
+		for (String digit : str.replaceAll("[^,0-9]", "").split(",")){
+			result[index++] = Integer.parseInt(digit);
+		}
+		return result;
+	}
+	
+	public String convert_penalties() {
+		String result = "[";
+		for (int i : penalties)
+			result += i + ",";
+		return result + "]";
+	}
+	
+	public String format() {
+		return this.login + " with " + good_answers + "/" +
 	asked_times + " (" + this.good_answers * 100 / this.asked_times + "%)";
 	}
 	
@@ -48,7 +83,16 @@ public class Student {
 		this.asked_times++; 
 	}
 	
-	public void save() {
-		
+	public String toString() {
+		String format = "";
+		format += this.login + " ";
+		format += this.asked_times + " ";
+		format += this.good_answers + " "; 
+		format += this.convert_penalties();
+		return format;
+	}
+	
+	public void save(PrintWriter pw) {
+		pw.println(this.toString());
 	}
 }
