a186f0d323b428046dd3e74185c033b4794d33b8
Antoine Allard <antoine.allard@epita.fr>
2018-09-13 22:13:09 +0200

    Implementing first shot of the buffer, first commands (help & quit)

---
+++ ACDC_Students/src/fr/fuizziy/acdc/Commands.java
@@ -0,0 +1,12 @@
+package fr.fuizziy.acdc;
+
+public class Commands {
+
+	public static void help() {
+		System.out.println("--- HELP ---");
+		System.out.println("ACDC 2021, sups tracker by Antoine Allard");
+		System.out.println("Arguments:"); 
+		System.out.println("quit : exits the software");  
+		System.out.println("help : displays these informations");  
+	}
+}
--- ACDC_Students/src/fr/fuizziy/acdc/Main.java
+++ ACDC_Students/src/fr/fuizziy/acdc/Main.java
@@ -1,12 +1,13 @@
 package fr.fuizziy.acdc;
   
 import java.io.File; 
-import java.io.IOException; 
+import java.io.IOException;
+import java.util.Scanner; 
 
 public class Main {
 	
 	public static File save;
-	
+ 
 	public static void main(String[] args) throws IOException {  
 		System.out.println("WELCOME. How will you annoy the sups today ?");  
 		save = new File("scores.txt"); 
@@ -15,8 +16,32 @@ public class Main {
 			System.out.println("Please configure it and come back.");
 			return;
 		}
-		Student.load_students(save); 
-		
+		Student.load_students(save);   
+		Scanner scanner = new Scanner(System.in);
+		String cmd;
+		System.out.print("Type 'help' for help & a list of commands."
+				+ "\nCommand: ");
+		while ((cmd = scanner.nextLine()) != "quit") {  
+			System.out.println(cmd);
+			String[] options = cmd.split(" "); 
+			if (cmd.replaceAll(" ", "").length() == 0) {
+				Commands.help();
+				System.out.print("\nCommand: ");
+				continue;
+			}
+			switch (options[0]) {
+				case "quit":
+					return; 
+				case "sanction":
+				case "s":
+					break;
+				default:
+					System.out.println("Invalid command");
+					break;
+			}
+			System.out.print("\nCommand: ");
+		};
+		scanner.close();
 		Student.save_students(save);
 	} 
 	
