f947afbe149179108d09409496afd52329466b69
Antoine Allard <antoine.allard@epita.fr>
2018-09-16 18:54:27 +0200

    Creating the save command and parametring the sanction one

--- ACDC_Students/src/fr/fuizziy/acdc/Commands.java
+++ ACDC_Students/src/fr/fuizziy/acdc/Commands.java
@@ -2,11 +2,53 @@ package fr.fuizziy.acdc;
 
 public class Commands {
 
+	public enum Sanction{
+		LATE(1),
+		MISSING(2),
+		KICKED_OUT(3),
+		FAIL_PUSH(4), 
+		BEHAVIOR(5);
+		
+		int index;
+		
+		Sanction(int index){
+			this.index = index;
+		}
+		
+		String print() {
+			return this.index + "->" + this.name();
+		}
+		
+		void apply(String login) {
+			if (!Student.students_class.containsKey(login)) {
+				System.out.println("This student does not exist...");
+				return;
+			}
+			Student student = Student.students_class.get(login);
+			student.add_penalty(index - 1);
+			System.out.println("Now has: " + student.convert_penalties());
+		}
+		 
+		static void print_all() {
+			for (int i = 0; i < Student.penalty_desc.length; i++)
+				System.out.println(Sanction.values()[i].print());
+		}
+	}
+	
 	public static void help() {
 		System.out.println("--- HELP ---");
 		System.out.println("ACDC 2021, sups tracker by Antoine Allard");
 		System.out.println("Arguments:"); 
 		System.out.println("quit : exits the software");  
 		System.out.println("help : displays these informations");  
+	}
+	
+	public static void apply_sanction(String[] args) {
+		int id_sanction = Integer.parseInt(args[2]) - 1;
+		if (id_sanction > Student.penalty_desc.length) {
+			System.out.println("index error. Please refer to the list above.");
+			return;
+		}
+		Sanction.values()[id_sanction].apply(args[1]);
 	}
 }
--- ACDC_Students/src/fr/fuizziy/acdc/Main.java
+++ ACDC_Students/src/fr/fuizziy/acdc/Main.java
@@ -21,8 +21,8 @@ public class Main {
 		String cmd;
 		System.out.print("Type 'help' for help & a list of commands."
 				+ "\nCommand: ");
+		loop_scanner:
 		while ((cmd = scanner.nextLine()) != "quit") {  
-			System.out.println(cmd);
 			String[] options = cmd.split(" "); 
 			if (cmd.replaceAll(" ", "").length() == 0) {
 				Commands.help();
@@ -30,10 +30,24 @@ public class Main {
 				continue;
 			}
 			switch (options[0]) {
+				case "save": 
+					Student.save_students(save);
+					break;
 				case "quit":
-					return; 
+					break loop_scanner;  
+				case "help":
+				case "h":
+					Commands.help();
+					break;
 				case "sanction":
 				case "s":
+					if (options.length != 3) {
+						System.out.println("format: sanction login 1-"
+											+ Student.penalty_desc.length);
+						Commands.Sanction.print_all();
+						break;
+					} 
+					Commands.apply_sanction(options);
 					break;
 				default:
 					System.out.println("Invalid command");
